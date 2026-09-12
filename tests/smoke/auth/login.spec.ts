9ac2a62233230f03d74553f9ec7770568fc23535
Shashank Shetty <shettyshashank089@gmail.com>
2026-09-13 04:29:04 +0530

    edited

--- tests/smoke/auth/login.spec.ts
+++ tests/smoke/auth/login.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@playwright/test';
-import { LoginPage } from '../../pages/LoginPage';
+import { LoginPage } from '../../../pages/LoginPage';
 
 test('Login Test@Smoke', async ({ page }) => {
   const loginPage = new LoginPage(page);
